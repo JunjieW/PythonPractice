f2cef5e9617c725f512b98f064a6fd86197c58f8
JunjieW <JunjieW@users.noreply.github.com>
2016-04-16 02:01:21 -0400

    10.2 Done

--- Coursera-Python-Data-Structures.java
+++ Coursera-Python-Data-Structures.java
@@ -121,3 +121,32 @@ for line in handle:
 		continue
 		
 print key, wordCount.get(key)
+
+
+########################################################################
+# 10.2 Write a program to read through the mbox-short.txt and figure out 
+# the distribution by hour of the day for each of the messages. You can 
+# pull the hour out from the 'From ' line by finding the time and then
+# splitting the string a second time using a colon.
+#            From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+# Once you have accumulated the counts for each hour, print out the 
+# counts, sorted by hour as shown below.
+########################################################################
+
+# The description is a litle ambiguous, see online test:
+# https://pr4e.dr-chuck.com/tsugi/mod/pythonauto/index.php?PHPSESSID=669851efb76f2236de32bd8aabd01548
+
+name = raw_input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+counter = dict()
+
+for line in handle:
+	if line.startswith('From ') :
+		h = line.split()[5].split(':')[0]
+		counter[h] = counter.get(h, 0) + 1
+		
+lst = sorted([(k,v) for k, v in counter.items()])
+for k, v in lst:
+    print k, v
